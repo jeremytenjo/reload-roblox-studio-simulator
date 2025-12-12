ccaf59dad2086d47417ec991c556a5d3f8be7326
jeremytenjo <tenjojeremy@gmail.com>
2025-12-12 11:07:39 -0500

    feat: enhance ESLint configuration and add linting scripts to package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,86 @@
+import { defineConfig } from 'eslint/config'
 import typescriptEslint from 'typescript-eslint'
+import eslintPluginRequireJsExtension from 'eslint-plugin-require-js-extension'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default [
+import importPlugin from 'eslint-plugin-import'
+import prettierConfig from './.prettierrc.js'
+
+export default defineConfig([
+  eslintPluginPrettierRecommended,
+
+  // Global settings
   {
-    files: ['**/*.ts'],
+    ignores: ['/out'],
   },
+
+  // eslint config
   {
-    plugins: {
-      '@typescript-eslint': typescriptEslint.plugin,
-    },
-
-    languageOptions: {
-      parser: typescriptEslint.parser,
-      ecmaVersion: 2022,
-      sourceType: 'module',
-    },
-
+    files: ['eslint.config.js'],
     rules: {
-      '@typescript-eslint/naming-convention': [
-        'warn',
+      'prettier/prettier': [
+        'error',
         {
-          selector: 'import',
-          format: ['camelCase', 'PascalCase'],
+          ...prettierConfig,
         },
       ],
-
-      curly: 'warn',
-      eqeqeq: 'warn',
-      'no-throw-literal': 'warn',
-      semi: 'warn',
     },
   },
-]
+
+  // Settings for Roblox TS source files
+  {
+    basePath: './',
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: typescriptEslint.parser,
+      parserOptions: {
+        jsx: true,
+        useJSXTextNode: true,
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint.plugin,
+      'require-js-extension': eslintPluginRequireJsExtension,
+      import: importPlugin,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          ...prettierConfig,
+        },
+      ],
+      'require-js-extension/require-js-extension': 'error',
+      '@typescript-eslint/consistent-type-imports': 2,
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-namespace': 0,
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/no-unused-vars': 1,
+      'no-useless-catch': 1,
+      'no-async-promise-executor': 0,
+      'react/jsx-uses-react': 0,
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+        },
+      ],
+      'prefer-const': 2,
+      'no-var': 2,
+      'no-const-assign': 1,
+      'no-this-before-super': 1,
+      'no-unreachable': 2,
+      'no-unneeded-ternary': 2,
+      'import/no-anonymous-default-export': 0,
+      'no-debugger': 1,
+      'no-console': 0,
+      'constructor-super': 1,
+      'valid-typeof': 1,
+      'arrow-body-style': ['error', 'always'],
+    },
+  },
+])
